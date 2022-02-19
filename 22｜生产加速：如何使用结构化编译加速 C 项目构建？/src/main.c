d70d5a0a89ae9093a87ff8c01b3ed935b07e3251
Becavalier <yhorg@hotmail.com>
2022-02-19 11:44:32 +0800

    Update code snippets.

---
+++ 22｜生产加速：如何使用结构化编译加速 C 项目构建？/src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <tgmath.h>
+#include "mod.h"
+int main(void) {
+  printf("%f\n", sqrt(fib(5)));
+  return 0;
+}
---
+++ 22｜生产加速：如何使用结构化编译加速 C 项目构建？/src/mod.c
@@ -0,0 +1,5 @@
+int fib(int n) {
+  if (n <= 1)
+    return n;
+  return fib(n - 1) + fib(n - 2);
+}
